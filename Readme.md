5e32187480089f7895da2a3d7cf40fecf256ccb6
Wizerd <Wizerd@recieved.email>
2023-12-15 17:06:46 +0800

    [init] 初始化项目

---
+++ Readme.md
@@ -0,0 +1,27 @@
+## 项目简介
+
+本项目是为了方便用户获取 PandoraNext License 使用情况而创建的，用户可以通过本项目获取到自己的 License 使用情况，以及用量的重置时间。
+
+## 用法
+
+1. git clone 本项目到本地
+
+2. 进入项目目录，修改 `docker-compose.yml` 中的 `PANDORA_LICENSE_ID` 变量为自己的 License ID
+
+3. 运行 `docker-compose up -d` 启动容器
+
+## 调用方法
+
+默认请求地址：`http://ip:53333/api/getPandoraNextLicUsage`
+
+预期响应：
+
+```json
+{
+    "current":<当前用量>,
+    "ip":"<授权IP>",
+    "license_id":"<证书ID>",
+    "total":"<总用量>",
+    "ttl":"1\u5c0f\u65f624\u5206\u949f1\u79d2" # 预期重置时间
+}
+```
---
+++ main.py
@@ -0,0 +1,40 @@
+from flask import Flask, jsonify, request, Response
+import requests
+import os
+import json
+
+app = Flask(__name__)
+
+@app.route('/api/getPandoraNextLicUsage', methods=['GET'])
+def get_pandora_next_lic_usage():
+    # 向 Pandora Next API 发起请求
+    response = requests.get(f'https://dash.pandoranext.com/api/{license_id}/usage')
+    if response.status_code == 200:
+        data = response.json()
+        
+        # 解析 TTL
+        ttl_seconds = int(data.get('ttl', 0))
+        hours = ttl_seconds // 3600
+        minutes = (ttl_seconds % 3600) // 60
+        seconds = ttl_seconds % 60
+        
+        # 更新数据
+        data['ttl'] = f"{hours}小时{minutes}分钟{seconds}秒"
+
+        # 保护license_id参数，仅显示前后3位
+        data['license_id'] = f"{license_id[:3]}***{license_id[-3:]}"
+
+
+        return jsonify(data)
+    else:
+        return jsonify({"error": "无法从 Pandora Next 获取数据"}), 500
+
+if __name__ == '__main__':
+    global license_id
+    license_id = os.getenv('PANDORA_LICENSE_ID')
+    print(f"License ID: {license_id}")
+    if not license_id:
+        # 退出
+        print("未设置 Pandora License ID 环境变量")
+        exit(1)
+    app.run(host='0.0.0.0', port=23333)
